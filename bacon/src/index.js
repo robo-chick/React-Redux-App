21f2faeab5f9e06a7a293869fb9d7b02d068d10a
Tasha <tashingsworth@gmail.com>
2020-06-23 10:57:54 -0400

    connected to store

--- bacon/src/index.js
+++ bacon/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {appReducer} from './reducers';
+import {Provider} from 'react-redux';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const logger = ({getState}) => next => action => {
+  console.log("Dispatching action:", action);
+  next(action);
+};
+
+const store = createStore(appReducer, applyMiddleware(logger, thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
